086f07ff1abab4d6e9b0de8bdf13fab25d18e255
ssssojung <1216thwjd@naver.com>
2023-01-18 00:46:30 +0900

    header&footer 아이콘 수정

--- src/component/Header.js
+++ src/component/Header.js
@@ -8,26 +8,26 @@ function Header(props) {
     return (
         
         <>
-            <div id='header'className='position-fixed px-5 py-3' >
-                <div className='d-flex justify-content-between align-items-center'>
-                    <div>
+            <div id='header'className='position-fixed px-5 pt-4' >
+                <div className='d-flex justify-content-end align-items-center'>
+                    <div className='logo'>
                         <img src="../../img/home/logo_white.svg" alt="logo" />
                     </div>
                     <a href="#none" className='btn_menu' onClick={handleToggle}>
-                        <span>메뉴 만들거</span>
+                        {isActive ? <img src="../../img/home/menu_icon.svg" alt="" /> : <i className='xi-close-thin'></i>}
                     </a>
                 </div>
             </div>
             <div id='menu' className={isActive ? "d-none" : "d-block"}>
-                    <div className='d-flex  fullImg text-white'style={{backgroundImage:"url(../../img/home/menu.jpg)"}}>
-                        <div className='col-6 flex-column ' >
-                            <h2 className=''>
+                    <div className='  fullImg text-white'style={{backgroundImage:"url(../../img/home/menu.jpg)"}}>
+                        <div className=' px-5 flex-column menuT ' >
+                            <h2 className='mb-5 pb-5'>
                                 ONE OBJECT, MORE VALUE<br/>
                                 <span>하나의 사물, 공간의 가치를 더하다.</span>
                             </h2>
-                            <a href='#arrivals'><p>NEW ARRIVALS</p></a>
-                            <a href='#shop'><p>SHOP</p></a>
-                            <a href='#brandstory'><p>BRAND STORY</p></a>
+                            <a href='#arrivals'><p className='py-5'>NEW ARRIVALS</p></a>
+                            <a href='#shop'><p className='py-5'>SHOP</p></a>
+                            <a href='#brandstory'><p className='py-5'>BRAND STORY</p></a>
                         </div>
                     </div>
             </div>
